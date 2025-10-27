52b3380753184717b922fb0f00254310820a99ce
AhmedRaza186 <sallumoon650@gmail.com>
2025-10-27 14:35:11 +0500

    updated counter

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 i = 0
-function counterplus(){
-i++
+j = 0
+
+function counterplus() {
+   i++
    document.querySelector('h2').innerText = i
+   if (i > 10) {
+      j++
+      document.querySelector('.laps p').innerText = j
+      reset()
+   }
+   document.querySelector('h2').style.color = 'green'
 }
-function counterminus(){
-i--
+function counterminus() {
+   i--
    document.querySelector('h2').innerText = i
+    document.querySelector('h2').style.color = 'red'
 }
-function reset(){
+function reset() {
    i = 0
    document.querySelector('h2').innerText = i
-
+      document.querySelector('h2').style.color = 'black'
+}
+function resetLaps() {
+   j = 0
+document.querySelector('.laps p').innerText = j
 }
